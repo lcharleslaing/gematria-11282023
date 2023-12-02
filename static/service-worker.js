b24654ffb496a31648dfb3bdf4e5f67d1fe758f5
Lee Charles Laing <71520336+lcharleslaing@users.noreply.github.com>
2023-12-01 20:19:16 -0500

    Add service worker for offline support

---
+++ static/service-worker.js
@@ -0,0 +1,8 @@
+// service-worker.js
+self.addEventListener('install', (event) => {
+    console.log('Service Worker installing.');
+});
+
+self.addEventListener('fetch', (event) => {
+    console.log('Fetching:', event.request.url);
+});
